11b82dfb1ca38f7983d2517fd606fc7d94e40bf9
buffet <niclas@countingsort.com>
2022-01-21 13:36:45 +0000

    Moved stack addresses into linker script

--- asm/crt0.s
+++ asm/crt0.s
@@ -5,19 +5,19 @@
 _start:
     mov r0, #PSR_IRQ_MODE
     msr cpsr_fc, r0
-    ldr sp, sp_irq
+    ldr sp, sp_irq_ptr
     mov r0, #PSR_SYS_MODE
     msr cpsr_fc, r0
-    ldr sp, sp_sys
+    ldr sp, sp_sys_ptr
     ldr r1, intr_vector_start
-    add r0, pc, #0x20 // intr_vector_start
+    add r0, pc, #0x20 @ intr_main
     str r0, [r1]
     ldr r1, main_loop_ptr
     mov lr, pc
     bx r1
     b _start
 
-sp_sys: .4byte 0x03007e60
-sp_irq: .4byte 0x03007fa0
+sp_sys_ptr: .4byte sp_sys
+sp_irq_ptr: .4byte sp_irq
 intr_vector_start: .4byte 0x03007ffc
 main_loop_ptr: .4byte 0x0800023d
