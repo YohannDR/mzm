051009cf9895123942d892e45f28a301883d4b32
buffet <niclas@countingsort.com>
2022-01-21 13:42:18 +0000

    Put intr_vector address into linker script

--- asm/crt0.s
+++ asm/crt0.s
@@ -9,7 +9,7 @@ _start:
     mov r0, #PSR_SYS_MODE
     msr cpsr_fc, r0
     ldr sp, sp_sys_ptr
-    ldr r1, intr_vector_start
+    ldr r1, intr_vector_ptr
     add r0, pc, #0x20 @ intr_main
     str r0, [r1]
     ldr r1, main_loop_ptr
@@ -17,7 +17,8 @@ _start:
     bx r1
     b _start
 
+@ pool
 sp_sys_ptr: .4byte sp_sys
 sp_irq_ptr: .4byte sp_irq
-intr_vector_start: .4byte 0x03007ffc
+intr_vector_ptr: .4byte intr_vector
 main_loop_ptr: .4byte 0x0800023d
