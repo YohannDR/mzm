05d42db16ec74b701ec7e4a41c3f91c2c1d8f25c
buffet <niclas@countingsort.com>
2022-01-24 19:24:38 +0000

    Add int types

---
+++ src/types.h
@@ -0,0 +1,11 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef unsigned char   u8;
+typedef   signed char   i8;
+typedef unsigned short u16;
+typedef   signed short i16;
+typedef unsigned int   u32;
+typedef   signed int   i32;
+
+#endif /* TYPES_H */
