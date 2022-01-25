571b4f7906b578617387c9bc72125b7be971d58c
buffet <niclas@countingsort.com>
2022-01-25 18:58:37 +0000

    Use macro in libgcc

--- asm/libgcc.s
+++ asm/libgcc.s
@@ -5,86 +5,72 @@ _call_via_r0:
     bx r0
     nop
 
-    .globl _call_via_r1
-    .type _call_via_r1, %function
+    thumb_func_start _call_via_r1
 _call_via_r1:
     bx r1
     nop
 
-    .globl _call_via_r2
-    .type _call_via_r2, %function
+    thumb_func_start _call_via_r2
 _call_via_r2:
     bx r2
     nop
 
-    .globl _call_via_r3
-    .type _call_via_r3, %function
+    thumb_func_start _call_via_r3
 _call_via_r3:
     bx r3
     nop
 
-    .globl _call_via_r4
-    .type _call_via_r4, %function
+    thumb_func_start _call_via_r4
 _call_via_r4:
     bx r4
     nop
 
-    .globl _call_via_r5
-    .type _call_via_r5, %function
+    thumb_func_start _call_via_r5
 _call_via_r5:
     bx r5
     nop
 
-    .globl _call_via_r6
-    .type _call_via_r6, %function
+    thumb_func_start _call_via_r6
 _call_via_r6:
     bx r6
     nop
 
-    .globl _call_via_r7
-    .type _call_via_r7, %function
+    thumb_func_start _call_via_r7
 _call_via_r7:
     bx r7
     nop
 
-    .globl _call_via_r8
-    .type _call_via_r8, %function
+    thumb_func_start _call_via_r8
 _call_via_r8:
     bx r8
     nop
 
-    .globl _call_via_r9
-    .type _call_via_r9, %function
+    thumb_func_start _call_via_r9
 _call_via_r9:
     bx r9
     nop
 
-    .globl _call_via_sl
-    .type _call_via_sl, %function
+    thumb_func_start _call_via_sl
 _call_via_sl:
     bx sl
     nop
 
-    .globl _call_via_fp
-    .type _call_via_fp, %function
+    thumb_func_start _call_via_fp
 _call_via_fp:
     bx fp
     nop
 
-    .globl _call_via_ip
-    .type _call_via_ip, %function
+    thumb_func_start _call_via_ip
 _call_via_ip:
     bx ip
     nop
 
-    .globl _call_via_sp
-    .type _call_via_sp, %function
+    thumb_func_start _call_via_sp
 _call_via_sp:
     bx sp
     nop
 
-    .globl _call_via_lr
-    .type _call_via_lr, %function
+    thumb_func_start _call_via_lr
 _call_via_lr:
     bx lr
     nop
