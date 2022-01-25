f458223d2f4e7691fcd5f9c8889bdaaf3017cdb0
buffet <niclas@countingsort.com>
2022-01-25 18:59:13 +0000

    Remove magic byte TODO

--- src/clear_ram.c
+++ src/clear_ram.c
@@ -3,7 +3,6 @@
 #include "gba_memory.h"
 
 void clear_ram(void) {
-    /* TODO: magic numbers (bit_size) */
     bit_fill(DMA_GENERAL_PURPOSE_COPY, 0x00, VRAM_BASE, VRAM_SIZE, 0x10);
     bit_fill(DMA_GENERAL_PURPOSE_COPY, 0xff, OAM_BASE, OAM_SIZE, 0x20);
     bit_fill(DMA_GENERAL_PURPOSE_COPY, 0x00, PALRAM_BASE, PALRAM_SIZE, 0x10);
--- src/load_intr_code.c
+++ src/load_intr_code.c
@@ -3,7 +3,6 @@
 #include "interrupts.h"
 
 void load_intr_code(void) {
-    /* TODO: magic number */
     dma_transfer(DMA_GENERAL_PURPOSE_COPY, intr_main, &intr_code, sizeof(intr_code), 0x10);
     intr_code_ptr = &intr_code;
 }
