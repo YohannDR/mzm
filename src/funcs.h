da1cbb73077b2ea473c34d0b3971b8e9a7761a2b
buffet <niclas@countingsort.com>
2022-01-28 15:51:49 +0000

    Fix funcs.h function's signatures

--- src/funcs.h
+++ src/funcs.h
@@ -3,12 +3,12 @@
 
 #include "types.h"
 
-void clear_ram();
+void clear_ram(void);
 void dma_transfer(int dma_channel, void *src, void *dst, u32 len, u8 bit_size);
-void init_sound();
-void load_intr_code();
-void read_sram();
-void softreset_vblank_callback();
-void update_input();
+void init_sound(void);
+void load_intr_code(void);
+void read_sram(void);
+void softreset_vblank_callback(void);
+void update_input(void);
 
 #endif /* FUNCS_H */
