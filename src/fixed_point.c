b17ef7dec58faca0d1030a7418abe958572e60aa
buffet <niclas@countingsort.com>
2022-03-23 16:34:02 +0000

    Improve fix_mul, fix_div

--- src/fixed_point.c
+++ src/fixed_point.c
@@ -3,14 +3,10 @@
 i16
 fix_mul(i16 a, i16 b)
 {
-    i32 res;
-
-    // XXX: super ugly sign extension
-    i32 a32 = a << 0x10;
-    i32 b32 = b;
-    a32 >>= 0x10;
-
-    res = a32 * b32;
+    i32 x = a;
+    i32 y = b;
+    i32 res = x;
+    res *= y;
     res /= 0x100;
     return res;
 }
@@ -18,12 +14,10 @@ fix_mul(i16 a, i16 b)
 i16
 fix_div(i16 a, i16 b)
 {
-    // XXX: super ugly sign extension
-    i32 a32 = a << 0x10;
-    i32 b32 = b;
-    a32 >>= 8;
+    i32 x = a;
+    i32 y = b;
 
-    return a32 / b32;
+    return (x << 8) / y;
 }
 
 i16
