a370c26cd68e64af036cd4c92e92cfa5e7139c80
buffet <niclas@countingsort.com>
2022-03-23 20:48:11 +0000

    Fix formatting

--- src/sub_08000c04.c
+++ src/sub_08000c04.c
@@ -4,7 +4,9 @@
 #include "memory.h"
 #include "types.h"
 
-void sub_08000c04(void) {
+void
+sub_08000c04(void)
+{
     u32 tmp;
     bit_fill(3, 0xffff, &unk_02038000, SRAM_SIZE, 16);
     if (unk_03000c1c == 0) {
