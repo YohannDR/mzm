78f9e208229e1c4e0c7c379ad01bcf782e5d9b9a
YohannDR <yohann.demaison-regne@orange.fr>
2022-08-06 19:39:35 +0200

    Elevator pad AI cleanup

--- src/sprites_AI/elevator_pad.c
+++ src/sprites_AI/elevator_pad.c
@@ -1,6 +1,78 @@
 #include "elevator_pad.h"
 #include "../globals.h"
 
+const u32 sElevatorPadGFX[136];
+const u16 sElevatorPadPAL[16];
+
+const u16 sElevatorPadOAM_Moving_Frame0[4] = {
+    0x1,
+    OBJ_SHAPE_HORIZONTAL | 0x0, OBJ_SIZE_32x8 | 0x1f0, OBJ_SPRITE_OAM | 0x200
+};
+
+const u16 sElevatorPadOAM_Moving_Frame1[4] = {
+    0x1,
+    OBJ_SHAPE_HORIZONTAL | 0x0, OBJ_SIZE_32x8 | 0x1f0, OBJ_SPRITE_OAM | 0x208
+};
+
+const u16 sElevatorPadOAM_Moving_Frame2[4] = {
+    0x1,
+    OBJ_SHAPE_HORIZONTAL | 0x0, 0x1f8, OBJ_SPRITE_OAM | 0x228
+};
+
+const u16 sElevatorPadOAM_Moving_Frame3[4] = {
+    0x1,
+    OBJ_SHAPE_HORIZONTAL | 0x0, 0x1f8, OBJ_SPRITE_OAM | 0x22a
+};
+
+const u16 sElevatorPadOAM_Idle_Frame1[4] = {
+    0x1,
+    OBJ_SHAPE_HORIZONTAL | 0x0, OBJ_SIZE_32x8 | 0x1f0, OBJ_SPRITE_OAM | 0x220
+};
+
+const u16 sElevatorPadOAM_Idle_Frame2[4] = {
+    0x1,
+    OBJ_SHAPE_HORIZONTAL | 0x0, OBJ_SIZE_32x8 | 0x1f0, OBJ_SPRITE_OAM | 0x204
+};
+
+const u16 sElevatorPadOAM_Idle_Frame3[4] = {
+    0x1,
+    OBJ_SHAPE_HORIZONTAL | 0x0, OBJ_SIZE_32x8 | 0x1f0, OBJ_SPRITE_OAM | 0x224
+};
+
+const struct FrameData sElevatorPadOAM_Moving[5] = {
+    sElevatorPadOAM_Moving_Frame0,
+    0x1,
+    sElevatorPadOAM_Moving_Frame1,
+    0x2,
+    sElevatorPadOAM_Moving_Frame2,
+    0x1,
+    sElevatorPadOAM_Moving_Frame3,
+    0x2,
+    NULL,
+    0x0
+};
+
+const struct FrameData sElevatorPadOAM_Idle[7] = {
+    sElevatorPadOAM_Moving_Frame0,
+    0x8,
+    sElevatorPadOAM_Idle_Frame1,
+    0x8,
+    sElevatorPadOAM_Idle_Frame2,
+    0x8,
+    sElevatorPadOAM_Idle_Frame3,
+    0x8,
+    sElevatorPadOAM_Idle_Frame2,
+    0x8,
+    sElevatorPadOAM_Idle_Frame1,
+    0x8,
+    NULL,
+    0x0
+};
+
+/**
+ * @brief 2872c | b8 | Elevator pad AI
+ * 
+ */
 void ElevatorPad(void)
 {
     gCurrentSprite.ignoreSamusCollisionTimer = 0x1;
@@ -10,34 +82,40 @@ void ElevatorPad(void)
         gCurrentSprite.properties |= SP_ALWAYS_ACTIVE;
         gCurrentSprite.samusCollision = SSC_NONE;
         gCurrentSprite.pose = 0x9;
+
         gCurrentSprite.drawDistanceTopOffset = 0x1;
         gCurrentSprite.drawDistanceBottomOffset = 0x8;
         gCurrentSprite.drawDistanceHorizontalOffset = 0x10;
+
         gCurrentSprite.hitboxTopOffset = -0x4;
         gCurrentSprite.hitboxBottomOffset = 0x4;
         gCurrentSprite.hitboxLeftOffset = -0x4;
         gCurrentSprite.hitboxRightOffset = -0x4;
-        gCurrentSprite.pOam = elevator_pad_oam_2e0fe8;
+
+        gCurrentSprite.pOam = sElevatorPadOAM_Idle;
         gCurrentSprite.animationDurationCounter = 0x0;
         gCurrentSprite.currentAnimationFrame = 0x0;
     }
 
     if (gSamusData.pose == SPOSE_USING_AN_ELEVATOR)
     {
+        // Sync position
         gCurrentSprite.yPosition = gSamusData.yPosition;
         gCurrentSprite.xPosition = gSamusData.xPosition;
-        if (gCurrentSprite.pOam == elevator_pad_oam_2e0fe8)
+        if (gCurrentSprite.pOam == sElevatorPadOAM_Idle)
         {
-            gCurrentSprite.pOam = elevator_pad_oam_2e0fc0;
+            // Set moving
+            gCurrentSprite.pOam = sElevatorPadOAM_Moving;
             gCurrentSprite.animationDurationCounter = 0x0;
             gCurrentSprite.currentAnimationFrame = 0x0;
         }
     }
     else
     {
-        if (gCurrentSprite.pOam == elevator_pad_oam_2e0fc0)
+        if (gCurrentSprite.pOam == sElevatorPadOAM_Moving)
         {
-            gCurrentSprite.pOam = elevator_pad_oam_2e0fe8;
+            // Set idle
+            gCurrentSprite.pOam = sElevatorPadOAM_Idle;
             gCurrentSprite.animationDurationCounter = 0x0;
             gCurrentSprite.currentAnimationFrame = 0x0;
         }
