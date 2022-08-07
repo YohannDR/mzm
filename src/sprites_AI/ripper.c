ac9a8476297d1e610424e23304aee08a6f019e0a
YohannDR <yohann.demaison-regne@orange.fr>
2022-08-07 19:27:34 +0200

    Ripper AI cleanup

--- src/sprites_AI/ripper.c
+++ src/sprites_AI/ripper.c
@@ -3,33 +3,112 @@
 #include "../../data/data.h"
 #include "../globals.h"
 
+const u32 sRipperGFX[141];
+const u16 sRipperPAL[16];
+const u32 sRipperPurpleGFX[141];
+const u16 sRipperPurplePAL[16];
+
+const u16 sRipperOAM_Moving_Frame0[4] = {
+    0x1,
+    OBJ_SHAPE_HORIZONTAL | 0xf3, OBJ_SIZE_32x16 | 0x1f0, OBJ_SPRITE_OAM | 0x200
+};
+
+const u16 sRipperOAM_Moving_Frame1[7] = {
+    0x2,
+    0xf3, OBJ_SIZE_16x16 | 0x1f0, OBJ_SPRITE_OAM | 0x204,
+    0xf3, OBJ_SIZE_16x16 | 0x0, OBJ_SPRITE_OAM | 0x202
+};
+
+const u16 sRipperOAM_Moving_Frame2[7] = {
+    0x2,
+    0xf3, OBJ_SIZE_16x16 | 0x1f0, OBJ_SPRITE_OAM | 0x206,
+    0xf3, OBJ_SIZE_16x16 | 0x0, OBJ_SPRITE_OAM | 0x202
+};
+
+const u16 sRipperOAM_TurningAround_Frame0[4] = {
+    0x1,
+    OBJ_SHAPE_HORIZONTAL | 0xf3, OBJ_SIZE_32x16 | 0x1f0, OBJ_SPRITE_OAM | 0x208
+};
+
+const u16 sRipperOAM_TurningAround_Frame1[4] = {
+    0x1,
+    0xf3, OBJ_SIZE_16x16 | 0x1f8, OBJ_SPRITE_OAM | 0x20c
+};
+
+const struct FrameData sRipperOAM_Moving[5] = {
+    sRipperOAM_Moving_Frame0,
+    0x10,
+    sRipperOAM_Moving_Frame1,
+    0x10,
+    sRipperOAM_Moving_Frame2,
+    0x10,
+    sRipperOAM_Moving_Frame1,
+    0x10,
+    NULL,
+    0x0
+};
+
+const struct FrameData sRipperOAM_TurningAround[3] = {
+    sRipperOAM_TurningAround_Frame0,
+    0x3,
+    sRipperOAM_TurningAround_Frame1,
+    0x3,
+    NULL,
+    0x0
+};
+
+const struct FrameData sRipperOAM_TurningAroundPart2[3] = {
+    sRipperOAM_TurningAround_Frame1,
+    0x3,
+    sRipperOAM_TurningAround_Frame0,
+    0x3,
+    NULL,
+    0x0
+};
+
+
+/**
+ * @brief 1bd8c | 70 | Initializes a ripper sprite
+ * 
+ */
 void RipperInit(void)
 {
     gCurrentSprite.hitboxTopOffset = -0x24;
     gCurrentSprite.hitboxBottomOffset = 0x0;
     gCurrentSprite.hitboxLeftOffset = -0x20;
     gCurrentSprite.hitboxRightOffset = 0x20;
+
     gCurrentSprite.drawDistanceTopOffset = 0x10;
     gCurrentSprite.drawDistanceBottomOffset = 0x8;
     gCurrentSprite.drawDistanceHorizontalOffset = 0x10;
-    gCurrentSprite.pOam = ripper_oam_2cc048;
+
+    gCurrentSprite.pOam = sRipperOAM_Moving;
     gCurrentSprite.animationDurationCounter = 0x0;
     gCurrentSprite.currentAnimationFrame = 0x0;
+
     gCurrentSprite.samusCollision = SSC_HURTS_SAMUS;
     gCurrentSprite.health = sPrimarySpriteStats[gCurrentSprite.spriteID][0];
     gCurrentSprite.yPosition -= 0x8;
     SpriteUtilChooseRandomXFlip();
-    gCurrentSprite.pose = 0x8;
+    gCurrentSprite.pose = RIPPER_POSE_MOVING_INIT;
 }
 
-void RipperGFXInit(void)
+/**
+ * @brief 1bdfc | 20 | Initializes a ripper to be moving
+ * 
+ */
+void RipperMovingInit(void)
 {
-    gCurrentSprite.pose = 0x9;
-    gCurrentSprite.pOam = ripper_oam_2cc048;
+    gCurrentSprite.pose = RIPPER_POSE_MOVING;
+    gCurrentSprite.pOam = sRipperOAM_Moving;
     gCurrentSprite.currentAnimationFrame = 0x0;
     gCurrentSprite.animationDurationCounter = 0x0;
 }
 
+/**
+ * @brief 1be1c | 6c | Handles a ripper sprite moving
+ * 
+ */
 void RipperMove(void)
 {
     u16 speed;
@@ -41,48 +120,66 @@ void RipperMove(void)
 
     if (gCurrentSprite.status & SPRITE_STATUS_XFLIP)
     {
+        // Move right
         SpriteUtilCheckCollisionAtPosition(gCurrentSprite.yPosition - 0x10, gCurrentSprite.xPosition + 0x24);
         if (gPreviousCollisionCheck != 0x11)
             gCurrentSprite.xPosition += speed;
         else
-            gCurrentSprite.pose = 0xA;
+            gCurrentSprite.pose = RIPPER_POSE_TURNING_AROUND_INIT;
     }
     else
     {
+        // Move left
         SpriteUtilCheckCollisionAtPosition(gCurrentSprite.yPosition - 0x10, gCurrentSprite.xPosition - 0x24);
         if (gPreviousCollisionCheck == 0x11)
-            gCurrentSprite.pose = 0xA;
+            gCurrentSprite.pose = RIPPER_POSE_TURNING_AROUND_INIT;
         else
             gCurrentSprite.xPosition -= speed;
     }
 }
 
-void RipperTurnAroundGFXInit(void)
+/**
+ * @brief 1be88 | 20 | Initializes a ripper to be turning around
+ * 
+ */
+void RipperTurnAroundInit(void)
 {
-    gCurrentSprite.pose = 0xB;
-    gCurrentSprite.pOam = ripper_oam_2cc070;
+    gCurrentSprite.pose = RIPPER_POSE_TURNING_AROUND_FIRST_PART;
+    gCurrentSprite.pOam = sRipperOAM_TurningAround;
     gCurrentSprite.currentAnimationFrame = 0x0;
     gCurrentSprite.animationDurationCounter = 0x0;
 }
 
-void RipperTurnAroundBegin(void)
+/**
+ * @brief 1bea8 | 38 | Handles the first part of a ripper turning around
+ * 
+ */
+void RipperTurnAroundFirstPart(void)
 {
     if (SpriteUtilCheckEndCurrentSpriteAnim())
     {
         gCurrentSprite.status ^= SPRITE_STATUS_XFLIP;
-        gCurrentSprite.pose = 0xC;
-        gCurrentSprite.pOam = ripper_oam_2cc088;
+        gCurrentSprite.pose = RIPPER_POSE_TURNING_AROUND_SECOND_PART;
+        gCurrentSprite.pOam = sRipperOAM_TurningAroundPart2;
         gCurrentSprite.animationDurationCounter = 0x0;
         gCurrentSprite.currentAnimationFrame = 0x0;
     }
 }
 
-void RipperCheckTurnedAround(void)
+/**
+ * @brief 1bee0 | 1c | Handles the second part of a ripper turning around
+ * 
+ */
+void RipperTurnAroundSecondPart(void)
 {
     if (SpriteUtilCheckNearEndCurrentSpriteAnim())
-        gCurrentSprite.pose = 0x8;
+        gCurrentSprite.pose = RIPPER_POSE_MOVING_INIT;
 }
 
+/**
+ * @brief 1befc | dc | Ripper AI
+ * 
+ */
 void Ripper(void)
 {
     if (gCurrentSprite.properties & SP_DAMAGED)
@@ -96,29 +193,28 @@ void Ripper(void)
         SpriteUtilUpdateFreezeTimer();
     else
     {
-        if (!SpriteUtilIsSpriteStunned())
+        if (SpriteUtilIsSpriteStunned())
+            return;
+        switch (gCurrentSprite.pose)
         {
-            switch (gCurrentSprite.pose)
-            {
-                case 0x0:
-                    RipperInit();
-                    break;
-                case 0x8:
-                    RipperGFXInit();
-                case 0x9:
-                    RipperMove();
-                    break;
-                case 0xA:
-                    RipperTurnAroundGFXInit();
-                case 0xB:
-                    RipperTurnAroundBegin();
-                    break;
-                case 0xC:
-                    RipperCheckTurnedAround();
-                    break;
-                default:
-                    SpriteUtilSpriteDeath(DEATH_NORMAL, gCurrentSprite.yPosition - 0x18, gCurrentSprite.xPosition, TRUE, PE_SPRITE_EXPLOSION_MEDIUM);
-            }
+            case 0x0:
+                RipperInit();
+                break;
+            case RIPPER_POSE_MOVING_INIT:
+                RipperMovingInit();
+            case RIPPER_POSE_MOVING:
+                RipperMove();
+                break;
+            case RIPPER_POSE_TURNING_AROUND_INIT:
+                RipperTurnAroundInit();
+            case RIPPER_POSE_TURNING_AROUND_FIRST_PART:
+                RipperTurnAroundFirstPart();
+                break;
+            case RIPPER_POSE_TURNING_AROUND_SECOND_PART:
+                RipperTurnAroundSecondPart();
+                break;
+            default:
+                SpriteUtilSpriteDeath(DEATH_NORMAL, gCurrentSprite.yPosition - 0x18, gCurrentSprite.xPosition, TRUE, PE_SPRITE_EXPLOSION_MEDIUM);
         }
     }
 }
--- src/sprites_AI/ripper.h
+++ src/sprites_AI/ripper.h
@@ -3,12 +3,18 @@
 
 #include "../types.h"
 
+#define RIPPER_POSE_MOVING_INIT 0x8
+#define RIPPER_POSE_MOVING 0x9
+#define RIPPER_POSE_TURNING_AROUND_INIT 0xA
+#define RIPPER_POSE_TURNING_AROUND_FIRST_PART 0xB
+#define RIPPER_POSE_TURNING_AROUND_SECOND_PART 0xC
+
 void RipperInit(void);
-void RipperGFXInit(void);
+void RipperMovingInit(void);
 void RipperMove(void);
-void RipperTurnAroundGFXInit(void);
-void RipperTurnAroundBegin(void);
-void RipperCheckTurnedAround(void);
+void RipperTurnAroundInit(void);
+void RipperTurnAroundFirstPart(void);
+void RipperTurnAroundSecondPart(void);
 void Ripper(void);
 
 #endif /* RIPPER_AI_H */
