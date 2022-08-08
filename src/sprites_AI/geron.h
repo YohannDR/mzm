6d429a0dad2b0f488c8dece958b606fdec7173e1
YohannDR <yohann.demaison-regne@orange.fr>
2022-08-08 16:58:39 +0200

    Full geron AI

--- src/sprites_AI/geron.h
+++ src/sprites_AI/geron.h
@@ -3,6 +3,11 @@
 
 #include "../types.h"
 
+#define GERON_POSE_IDLE 0x9
+#define GERON_POSE_GETTING_DESTROYED 0x23
+#define GERON_POSE_DELAY_BEFORE_DESTROYED 0x25
+#define GERON_POSE_DESTROYED 0x61
+
 void Geron(void);
 
 #endif /* GERON_AI_H */
