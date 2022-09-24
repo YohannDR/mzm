a0a74b6e1e8d5fa1bcd765d1c807f9d30ccfcb2f
YohannDR <yohann.demaison-regne@orange.fr>
2022-09-24 22:19:02 +0200

    Add rm data in makefile

--- tools/extractor.py
+++ tools/extractor.py
@@ -3,12 +3,12 @@ from io import BufferedReader
 import os
 import shutil
 
-DATA_PATH = "../data/"
+DATA_PATH = "data/"
 subDirs: array = [
     "sprites/"
 ]
 
-shutil.rmtree(DATA_PATH, ignore_errors=False, onerror=None)
+# shutil.rmtree(DATA_PATH, ignore_errors=False, onerror=None)
 
 if not os.path.exists(DATA_PATH):
     # Create directories
@@ -17,19 +17,21 @@ if not os.path.exists(DATA_PATH):
         os.mkdir(DATA_PATH.__add__(dir))
 
 
-rom: BufferedReader = open("../mzm_us_baserom.gba", "rb")
-db: BufferedReader = open("../database.txt", "r")
+rom: BufferedReader = open("mzm_us_baserom.gba", "rb")
+db: BufferedReader = open("database.txt", "r")
 
 line: str = db.readline()
 while line != '':
-    # Formatted as follows : name;size;address
-    info: array = line.split(";")
-    rom.seek(int(info[2], 16))
-    data = rom.read(int(info[1]))
-    
-    output: BufferedReader = open("../data/".__add__(info[0]), "wb")
-    output.write(bytearray(data))
-    output.close()
+    # Formatted as follows : name;length;address
+    # The symbol # can be used as the first character of a line to make the extractor ignore it
+    if line[0] != '\n' and line[0] != '#':
+        info: array = line.split(";")
+        rom.seek(int(info[2], 16))
+        data = rom.read(int(info[1]))
+        
+        output: BufferedReader = open(DATA_PATH.__add__(info[0]), "wb")
+        output.write(bytearray(data))
+        output.close()
     line = db.readline()
 
 rom.close()
