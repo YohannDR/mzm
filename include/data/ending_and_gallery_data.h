bcb41082e0bfdaf950b40bd3571a3647c31f51b9
y.demaisonregne <yohann.demaison-regne@orange.fr>
2023-03-10 00:58:03 +0100

    Decompile more ending_and_gallery.c

--- include/data/ending_and_gallery_data.h
+++ include/data/ending_and_gallery_data.h
@@ -26,4 +26,24 @@ extern const u32 sEndingImageTextJapGFX[511];
 
 extern const u32 sEndingImageNumbersMiscGFX[218];
 
+// ...
+
+extern const u16 sCreditsCharactersPAL[3 * 16];
+extern const u16 sCreditsChozoWallPAL[15 * 16];
+
+// ...
+
+extern const u32 sCreditsCharactersGFX[615];
+
+// ...
+
+extern const u32 sCreditsChozoWallBackgroundGFX[7710];
+extern const u32 sCreditsChozoWallBackgroundTileTable[551];
+extern const u32 sCreditsChozoWallBottomGFX[3909];
+extern const u32 sCreditsChozoWallBottomTileTable[307];
+extern const u32 sCreditsChozoWallBottomZoomedGFX[5160];
+extern const u32 sCreditsChozoWallBottomZoomedTileTable[368];
+extern const u32 sCreditsChozoDrawingGFX[4770];
+extern const u32 sCreditsChozoDrawingTileTable[368];
+
 #endif /* ENDING_AND_GALLERY_DATA_H */
