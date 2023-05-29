67627b41461b2883c575f3e3410d59e3ac9e5cce
y.demaisonregne <yohann.demaison-regne@orange.fr>
2023-05-29 21:41:09 +0200

    Finish defining brinstar rooms data

--- include/data/rooms/brinstar_rooms_data.h
+++ include/data/rooms/brinstar_rooms_data.h
@@ -76,10 +76,247 @@ extern const u8 sBrinstar_8_Bg0[205];
 
 extern const u8 sBrinstar_4_Scrolls[SCROLL_DATA_SIZE(2)];
 extern const u8 sBrinstar_9_Clipdata[313];
-extern const u8 sBrinstar_9_Bg2[248];
+extern const u8 sBrinstar_9_Bg2[247];
 extern const u8 sBrinstar_9_Spriteset1[ENEMY_ROOM_DATA_ARRAY_SIZE(2)];
 extern const u8 sBrinstar_9_Bg1[1389];
 extern const u8 sBrinstar_9_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(1)];
 extern const u8 sBrinstar_9_Bg0[172];
+
+
+extern const u8 sBrinstar_5_Scrolls[SCROLL_DATA_SIZE(1)];
+extern const u8 sBrinstar_10_Clipdata[91];
+extern const u8 sBrinstar_10_Bg2[260];
+extern const u8 sBrinstar_10_Bg1[678];
+extern const u8 sBrinstar_10_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(3)];
+
+
+extern const u8 sBrinstar_6_Scrolls[SCROLL_DATA_SIZE(1)];
+extern const u8 sBrinstar_11_Clipdata[108];
+extern const u8 sBrinstar_11_Bg2[122];
+extern const u8 sBrinstar_11_Bg1[280];
+extern const u8 sBrinstar_11_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(2)];
+
+
+extern const u8 sBrinstar_12_Clipdata[379];
+extern const u8 sBrinstar_12_Bg2[337];
+extern const u8 sBrinstar_12_Bg1[1226];
+extern const u8 sBrinstar_12_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(5)];
+extern const u8 sBrinstar_12_Bg0[203];
+
+
+extern const u8 sBrinstar_13_Clipdata[206];
+extern const u8 sBrinstar_13_Bg2[78];
+extern const u8 sBrinstar_13_Bg1[520];
+extern const u8 sBrinstar_13_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(8)];
+extern const u8 sBrinstar_13_Bg0[220];
+
+
+extern const u8 sBrinstar_7_Scrolls[SCROLL_DATA_SIZE(2)];
+extern const u8 sBrinstar_14_Clipdata[212];
+extern const u8 sBrinstar_14_Bg2[38];
+extern const u8 sBrinstar_14_Bg1[532];
+extern const u8 sBrinstar_14_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(2)];
+
+
+extern const u8 sBrinstar_8_Scrolls[SCROLL_DATA_SIZE(3)];
+extern const u8 sBrinstar_15_Clipdata[287];
+extern const u8 sBrinstar_15_Bg2[78];
+extern const u8 sBrinstar_15_Bg1[975];
+extern const u8 sBrinstar_15_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(4)];
+
+
+extern const u8 sBrinstar_9_Scrolls[SCROLL_DATA_SIZE(3)];
+extern const u8 sBrinstar_16_Clipdata[191];
+extern const u8 sBrinstar_16_Bg2[40];
+extern const u8 sBrinstar_16_Bg1[512];
+extern const u8 sBrinstar_16_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(13)];
+
+
+extern const u8 sBrinstar_17_Clipdata[71];
+extern const u8 sBrinstar_17_Bg2[89];
+extern const u8 sBrinstar_17_Bg1[265];
+extern const u8 sBrinstar_17_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(6)];
+
+
+extern const u8 sBrinstar_18_Clipdata[52];
+extern const u8 sBrinstar_18_Bg2[172];
+extern const u8 sBrinstar_18_Bg1[135];
+extern const u8 sBrinstar_18_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(3)];
+
+
+extern const u8 sBrinstar_19_Clipdata[325];
+extern const u8 sBrinstar_19_Bg2[42];
+extern const u8 sBrinstar_19_Spriteset1[ENEMY_ROOM_DATA_ARRAY_SIZE(7)];
+extern const u8 sBrinstar_19_Bg1[935];
+extern const u8 sBrinstar_19_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(5)];
+extern const u8 sBrinstar_19_Bg0[274];
+
+
+extern const u8 sBrinstar_20_Clipdata[336];
+extern const u8 sBrinstar_20_Bg2[211];
+extern const u8 sBrinstar_20_Bg1[1163];
+extern const u8 sBrinstar_20_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(6)];
+
+
+extern const u8 sBrinstar_10_Scrolls[SCROLL_DATA_SIZE(2)];
+extern const u8 sBrinstar_21_Clipdata[241];
+extern const u8 sBrinstar_21_Bg2[174];
+extern const u8 sBrinstar_21_Spriteset1[ENEMY_ROOM_DATA_ARRAY_SIZE(9)];
+extern const u8 sBrinstar_21_Bg1[1038];
+extern const u8 sBrinstar_21_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(4)];
+extern const u8 sBrinstar_21_Bg0[394];
+
+
+extern const u8 sBrinstar_11_Scrolls[SCROLL_DATA_SIZE(2)];
+extern const u8 sBrinstar_22_Clipdata[172];
+extern const u8 sBrinstar_22_Bg2[50];
+extern const u8 sBrinstar_22_Spriteset1[ENEMY_ROOM_DATA_ARRAY_SIZE(9)];
+extern const u8 sBrinstar_22_Bg1[644];
+extern const u8 sBrinstar_22_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(3)];
+extern const u8 sBrinstar_22_Bg0[93];
+
+
+extern const u8 sBrinstar_12_Scrolls[SCROLL_DATA_SIZE(2)];
+extern const u8 sBrinstar_23_Clipdata[312];
+extern const u8 sBrinstar_23_Bg2[82];
+extern const u8 sBrinstar_23_Bg1[732];
+extern const u8 sBrinstar_23_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(17)];
+extern const u8 sBrinstar_23_Bg0[669];
+
+
+extern const u8 sBrinstar_13_Scrolls[SCROLL_DATA_SIZE(2)];
+extern const u8 sBrinstar_24_Clipdata[118];
+extern const u8 sBrinstar_24_Bg2[140];
+extern const u8 sBrinstar_24_Bg1[417];
+extern const u8 sBrinstar_24_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(5)];
+extern const u8 sBrinstar_24_Bg0[300];
+
+
+extern const u8 sBrinstar_14_Scrolls[SCROLL_DATA_SIZE(2)];
+extern const u8 sBrinstar_25_Clipdata[91];
+extern const u8 sBrinstar_25_Bg2[286];
+extern const u8 sBrinstar_25_Bg1[247];
+extern const u8 sBrinstar_25_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(2)];
+
+
+extern const u8 sBrinstar_15_Scrolls[SCROLL_DATA_SIZE(1)];
+extern const u8 sBrinstar_26_Clipdata[108];
+extern const u8 sBrinstar_26_Bg2[166];
+extern const u8 sBrinstar_26_Bg1[340];
+extern const u8 sBrinstar_26_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(2)];
+
+
+extern const u8 sBrinstar_27_Clipdata[52];
+extern const u8 sBrinstar_27_Bg2[156];
+extern const u8 sBrinstar_27_Bg1[141];
+extern const u8 sBrinstar_27_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(2)];
+
+
+extern const u8 sBrinstar_28_Clipdata[213];
+extern const u8 sBrinstar_28_Spriteset2[ENEMY_ROOM_DATA_ARRAY_SIZE(13)];
+extern const u8 sBrinstar_28_Bg2[107];
+extern const u8 sBrinstar_28_Spriteset1[ENEMY_ROOM_DATA_ARRAY_SIZE(8)];
+extern const u8 sBrinstar_28_Bg1[746];
+extern const u8 sBrinstar_28_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(11)];
+extern const u8 sBrinstar_28_Bg0[94];
+
+
+extern const u8 sBrinstar_16_Scrolls[SCROLL_DATA_SIZE(2)];
+extern const u8 sBrinstar_29_Clipdata[113];
+extern const u8 sBrinstar_29_Bg2[14];
+extern const u8 sBrinstar_29_Bg1[287];
+extern const u8 sBrinstar_29_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(1)];
+extern const u8 sBrinstar_29_Bg0[287];
+
+
+extern const u8 sBrinstar_30_Clipdata[76];
+extern const u8 sBrinstar_30_Bg2[14];
+extern const u8 sBrinstar_30_Bg1[220];
+extern const u8 sBrinstar_30_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(2)];
+
+
+extern const u8 sBrinstar_17_Scrolls[SCROLL_DATA_SIZE(1)];
+extern const u8 sBrinstar_31_Clipdata[136];
+extern const u8 sBrinstar_31_Bg2[14];
+extern const u8 sBrinstar_31_Bg1[408];
+extern const u8 sBrinstar_31_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(5)];
+
+
+extern const u8 sBrinstar_32_Clipdata[54];
+extern const u8 sBrinstar_32_Bg2[14];
+extern const u8 sBrinstar_32_Bg1[190];
+extern const u8 sBrinstar_32_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(2)];
+
+
+extern const u8 sBrinstar_33_Clipdata[71];
+extern const u8 sBrinstar_33_Bg2[14];
+extern const u8 sBrinstar_33_Bg1[211];
+extern const u8 sBrinstar_33_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(2)];
+
+
+extern const u8 sBrinstar_34_Clipdata[69];
+extern const u8 sBrinstar_34_Bg2[14];
+extern const u8 sBrinstar_34_Bg1[213];
+extern const u8 sBrinstar_34_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(2)];
+
+
+extern const u8 sBrinstar_35_Clipdata[123];
+extern const u8 sBrinstar_35_Bg2[58];
+extern const u8 sBrinstar_35_Bg1[482];
+extern const u8 sBrinstar_35_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(3)];
+
+
+extern const u8 sBrinstar_36_Clipdata[70];
+extern const u8 sBrinstar_36_Bg2[14];
+extern const u8 sBrinstar_36_Bg1[215];
+extern const u8 sBrinstar_36_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(2)];
+
+
+extern const u8 sBrinstar_37_Clipdata[52];
+extern const u8 sBrinstar_37_Bg2[168];
+extern const u8 sBrinstar_37_Bg1[137];
+extern const u8 sBrinstar_37_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(2)];
+
+
+extern const u8 sBrinstar_18_Scrolls[SCROLL_DATA_SIZE(2)];
+extern const u8 sBrinstar_38_Clipdata[153];
+extern const u8 sBrinstar_38_Bg2[102];
+extern const u8 sBrinstar_38_Spriteset1[ENEMY_ROOM_DATA_ARRAY_SIZE(7)];
+extern const u8 sBrinstar_38_Bg1[578];
+extern const u8 sBrinstar_38_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(5)];
+extern const u8 sBrinstar_38_Bg0[213];
+
+
+extern const u8 sBrinstar_39_Clipdata[78];
+extern const u8 sBrinstar_39_Bg2[14];
+extern const u8 sBrinstar_39_Bg1[216];
+extern const u8 sBrinstar_39_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(2)];
+
+
+extern const u8 sBrinstar_40_Clipdata[93];
+extern const u8 sBrinstar_40_Bg2[14];
+extern const u8 sBrinstar_40_Bg1[236];
+extern const u8 sBrinstar_40_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(3)];
+
+
+extern const u8 sBrinstar_41_Clipdata[214];
+extern const u8 sBrinstar_41_Bg2[100];
+extern const u8 sBrinstar_41_Spriteset1[ENEMY_ROOM_DATA_ARRAY_SIZE(18)];
+extern const u8 sBrinstar_41_Bg1[765];
+extern const u8 sBrinstar_41_Spriteset0[ENEMY_ROOM_DATA_ARRAY_SIZE(17)];
+extern const u8 sBrinstar_41_Bg0[48];
+
+
+extern const u8 sBrinstar_Bg3_0[1564];
+extern const u8 sBrinstar_Bg3_1[1600];
+extern const u8 sBrinstar_Bg3_2[1756];
+extern const u8 sBrinstar_Bg3_3[1360];
+extern const u8 sBrinstar_Bg3_4[1620];
+extern const u8 sBrinstar_Bg3_5[2168];
+extern const u8 sBrinstar_Bg3_6[928];
+extern const u8 sBrinstar_Bg3_7[2020];
+extern const u8 sBrinstar_Bg3_8[1164];
+extern const u8 sBrinstar_Bg3_9[1360];
+extern const u8 sBrinstar_Bg3_10[1200];
 
 #endif /* BRINSTAR_ROOMS_DATA_H */
