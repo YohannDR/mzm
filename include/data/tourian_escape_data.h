66752b6699147334897ce606581f9238c8e8dc8e
y.demaisonregne <yohann.demaison-regne@orange.fr>
2023-07-07 17:12:43 +0200

    Re-organize tourian escape data

--- include/data/tourian_escape_data.h
+++ include/data/tourian_escape_data.h
@@ -5,16 +5,29 @@
 #include "oam.h"
 #include "macros.h"
 
-// Temp
 extern const u16 sTourianEscape_375cc4[OAM_DATA_SIZE(12)];
 
-extern const u16* const sTourianEscape_47ce00[4];
-extern const u16* const sTourianEscape_47ce10[4];
-extern const u16* const sTourianEscape_47cea0[8];
-extern const u16* const sTourianEscape_47cec0[4];
-extern const u16* const sTourianEscape_47cf70[6];
-extern const u16* const sTourianEscape_47cf88[7];
-extern const u16* const sTourianEscape_47cfe4[4];
+extern const u16 sTourianEscapeExplodingPal[9 * 16];
+extern const u16 sMotherShipBlowingUpExplosionsPal[6 * 16];
+
+extern const u16 sTourianEscapeSamusInHerShipPowerSuitPal[7 * 16];
+extern const u16 sTourianEscapeSamusInHerShipVariaSuitPal[7 * 16];
+extern const u16 sTourianEscapeSamusInHerShipSuitlessPal[8 * 16];
+
+extern const u16 sTourianEscapeSamusLookingAroundPal[7 * 16];
+extern const u16 sTourianEscapeSamusChasedBackgroundPal[5 * 16];
+extern const u16 sTourianEscapeSamusGettingShotPal[5 * 16];
+extern const u16 sTourianEscapeSamusSurroundedPal[16 * 16];
+extern const u16 sTourianEscapeSamusChasedShipsPal[6 * 16];
+extern const u16 sTourianEscapeSamusChasedByPiratesFiringPal[4 * 16];
+extern const u16 sTourianEscapeSamusGoingToCrashPal[7 * 16];
+extern const u16 sTourianEscape_479ee0[1 * 16];
+extern const u16 sTourianEscape_479f00[1 * 16];
+extern const u16 sTourianEscapeSamusCrashingForegroundPal[3 * 16];
+extern const u16 sTourianEscape_479f80[7 * 16];
+extern const u16 sTourianEscapeShipCrashingBackgroundAndShipPal[16 * 16];
+extern const u16 sTourianEscapeSamusLookingAtSkyPal[10 * 16];
+extern const u16 sTourianEscapeSamusLookingAtMotherShipPal[10 * 16];
 
 extern const u16 sTourianEscape_47a4e0[OAM_DATA_SIZE(6)];
 extern const u16 sTourianEscape_47a506[OAM_DATA_SIZE(6)];
@@ -30,13 +43,79 @@ extern const u16 sTourianEscape_47a602[OAM_DATA_SIZE(5)];
 extern const u16 sTourianEscape_47a622[OAM_DATA_SIZE(5)];
 extern const u16 sTourianEscape_47a642[OAM_DATA_SIZE(5)];
 extern const u16 sTourianEscape_47a662[OAM_DATA_SIZE(5)];
+
+extern const u16 sTourianEscape_47a682[OAM_DATA_SIZE(1)];
+extern const u16 sTourianEscape_47a68a[OAM_DATA_SIZE(1)];
+extern const u16 sTourianEscape_47a692[OAM_DATA_SIZE(1)];
+extern const u16 sTourianEscape_47a69a[OAM_DATA_SIZE(1)];
+extern const u16 sTourianEscape_47a6a2[OAM_DATA_SIZE(1)];
+extern const u16 sTourianEscape_47a6aa[OAM_DATA_SIZE(1)];
+extern const u16 sTourianEscape_47a6b2[OAM_DATA_SIZE(1)];
+extern const u16 sTourianEscape_47a6ba[OAM_DATA_SIZE(1)];
+
+extern const u16 sTourianEscape_47a6c2[OAM_DATA_SIZE(4)];
+extern const u16 sTourianEscape_47a6dc[OAM_DATA_SIZE(4)];
+extern const u16 sTourianEscape_47a6f8[OAM_DATA_SIZE(4)];
+extern const u16 sTourianEscape_47a710[OAM_DATA_SIZE(4)];
+extern const u16 sTourianEscape_47a72a[OAM_DATA_SIZE(4)];
+extern const u16 sTourianEscape_47a744[OAM_DATA_SIZE(4)];
+extern const u16 sTourianEscape_47a75e[OAM_DATA_SIZE(4)];
+
+extern const u16 sTourianEscape_47a778[OAM_DATA_SIZE(8)];
+extern const u16 sTourianEscape_47a7aa[OAM_DATA_SIZE(10)];
+extern const u16 sTourianEscape_47a7e8[OAM_DATA_SIZE(12)];
+extern const u16 sTourianEscape_47a832[OAM_DATA_SIZE(14)];
+extern const u16 sTourianEscape_47a888[OAM_DATA_SIZE(11)];
+extern const u16 sTourianEscape_47a8cc[OAM_DATA_SIZE(9)];
+
 extern const u16 sTourianEscape_47a904[OAM_DATA_SIZE(3)];
+extern const u16 sTourianEscape_47a918[OAM_DATA_SIZE(1)];
+extern const u16 sTourianEscape_47a920[OAM_DATA_SIZE(1)];
+extern const u16 sTourianEscape_47a928[OAM_DATA_SIZE(1)];
+extern const u16 sTourianEscape_47a930[OAM_DATA_SIZE(1)];
+extern const u16 sTourianEscape_47a938[OAM_DATA_SIZE(1)];
+extern const u16 sTourianEscape_47a940[OAM_DATA_SIZE(1)];
+extern const u16 sTourianEscape_47a948[OAM_DATA_SIZE(1)];
+extern const u16 sTourianEscape_47a950[OAM_DATA_SIZE(1)];
+
 extern const u16 sTourianEscape_47a958[OAM_DATA_SIZE(8)];
 extern const u16 sTourianEscape_47a98a[OAM_DATA_SIZE(2)];
-extern const u16 sTourianEscape_47aa96[OAM_DATA_SIZE(18)];
-extern const u16 sTourianEscape_47ab28[OAM_DATA_SIZE(18)];
+extern const u16 sTourianEscape_47a998[OAM_DATA_SIZE(1)];
+extern const u16 sTourianEscape_47a9a0[OAM_DATA_SIZE(1)];
+extern const u16 sTourianEscape_47a9a8[OAM_DATA_SIZE(1)];
+
+extern const u16 sTourianEscape_47a9b0[OAM_DATA_SIZE(7)];
+extern const u16 sTourianEscape_47a9dc[OAM_DATA_SIZE(10)];
+extern const u16 sTourianEscape_47aa1a[OAM_DATA_SIZE(11)];
+extern const u16 sTourianEscape_47aa5e[OAM_DATA_SIZE(9)];
+
+extern const u16 sTourianEscape_47aa96[OAM_DATA_SIZE(24)];
+extern const u16 sTourianEscape_47ab28[OAM_DATA_SIZE(24)];
 extern const u16 sTourianEscape_47abba[OAM_DATA_SIZE(2)];
 extern const u16 sTourianEscape_47abc8[OAM_DATA_SIZE(2)];
+
+extern const u32 sTourianEscapeRainGfx[14];
+extern const u16 sTourianEscape_47ac10[OAM_DATA_SIZE(1)];
+extern const u16 sTourianEscape_47ac18[OAM_DATA_SIZE(2)];
+extern const u16 sTourianEscapeRainPal[1 * 16];
+
+
+
+
+
+
+
+extern const u16* const sTourianEscape_47ce00[4];
+extern const u16* const sTourianEscape_47ce10[4];
+extern const u16* const sTourianEscape_47cea0[8];
+extern const u16* const sTourianEscape_47cec0[4];
+extern const u16* const sTourianEscape_47cf70[6];
+extern const u16* const sTourianEscape_47cf88[7];
+extern const u16* const sTourianEscape_47cfe4[4];
+
+
+
+
 
 extern const struct FrameData sTourianEscape_47cbe4[16];
 extern const struct FrameData sTourianEscape_47cc64[11];
@@ -64,35 +143,6 @@ extern const u16 sTourianEscape_47cffe[11];
 // ...
 
 extern const u32 sTourianEscapeDebrisGfx[25];
-
-// ...
-
-extern const u16 sMotherShipBlowingUpExplosionsPal[6 * 16];
-
-// ...
-
-extern const u16 sTourianEscapeExplodingPal[9 * 16];
-
-// ...
-
-extern const u16 sTourianEscapeSamusInHerShipPowerSuitPal[7 * 16];
-extern const u16 sTourianEscapeSamusInHerShipVariaSuitPal[7 * 16];
-extern const u16 sTourianEscapeSamusInHerShipSuitlessPal[8 * 16];
-
-extern const u16 sTourianEscapeSamusLookingAroundPal[7 * 16];
-extern const u16 sTourianEscapeSamusChasedBackgroundPal[5 * 16];
-extern const u16 sTourianEscapeSamusGettingShotPal[5 * 16];
-extern const u16 sTourianEscapeSamusSurroundedPal[16 * 16];
-extern const u16 sTourianEscapeSamusChasedShipsPal[6 * 16];
-extern const u16 sTourianEscapeSamusChasedByPiratesFiringPal[4 * 16];
-extern const u16 sTourianEscapeSamusGoingToCrashPal[7 * 16];
-extern const u16 sTourianEscape_479ee0[1 * 16];
-extern const u16 sTourianEscape_479f00[1 * 16];
-extern const u16 sTourianEscapeSamusCrashingForegroundPal[3 * 16];
-extern const u16 sTourianEscape_479f80[7 * 16];
-extern const u16 sTourianEscapeShipCrashingBackgroundAndShipPal[16 * 16];
-extern const u16 sTourianEscapeSamusLookingAtSkyPal[10 * 16];
-extern const u16 sTourianEscapeSamusLookingAtMotherShipPal[10 * 16];
 
 // ...
 
@@ -149,10 +199,5 @@ extern const u32 sTourianEscapeSamusLookingAtMotherShipGfx[2153];
 extern const u32 sTourianEscapeSamusLookingAtMotherShipTileTable[577];
 extern const u32 sTourianEscapeSamusLookingAtMotherShipMotherShipGfx[3112];
 extern const u32 sTourianEscapeSamusLookingAtMotherShipMotherShipTileTable[577];
-
-// ...
-
-extern const u32 sTourianEscapeRainGfx[14];
-extern const u16 sTourianEscapeRainPal[1 * 16];
 
 #endif /* TOURIAN_ESCAPE_DATA_H */
